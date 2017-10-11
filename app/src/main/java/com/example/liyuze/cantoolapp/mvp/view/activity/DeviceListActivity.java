4060a7acc9e76fcf99644dee4ac4c64d2a5459f7
liyuze <409654850@qq.com>
2017-10-12 00:15:26 +0800

    界面更新

---
+++ app/src/main/java/com/example/liyuze/cantoolapp/mvp/view/activity/DeviceListActivity.java
@@ -0,0 +1,15 @@
+package com.example.liyuze.cantoolapp.mvp.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.liyuze.cantoolapp.R;
+
+public class DeviceListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+    }
+}
