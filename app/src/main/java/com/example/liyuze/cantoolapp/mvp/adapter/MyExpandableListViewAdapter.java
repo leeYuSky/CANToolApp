d3516c36c3313f745b3f0e76efbe1e594fd96a42
liyuze <409654850@qq.com>
2017-10-22 16:15:56 +0800

    datafragment 二级列表

---
+++ app/src/main/java/com/example/liyuze/cantoolapp/mvp/adapter/MyExpandableListViewAdapter.java
@@ -0,0 +1,121 @@
+package com.example.liyuze.cantoolapp.mvp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.liyuze.cantoolapp.R;
+import com.example.liyuze.cantoolapp.mvp.model.signal;
+import com.example.liyuze.cantoolapp.mvp.view.activity.MainActivity;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by liyuze on 17/10/22.
+ */
+public class MyExpandableListViewAdapter extends BaseExpandableListAdapter {
+
+    Context context;
+    Map<String, List<signal>> dataset;
+    String[] parentList;
+
+    public MyExpandableListViewAdapter(Context context,Map<String, List<signal>> dataset,String[] parentList){
+        this.context = context;
+        this.dataset = dataset;
+        this.parentList = parentList;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return dataset.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return dataset.get(parentList[groupPosition]).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return dataset.get(parentList[groupPosition]);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return dataset.get(parentList[groupPosition]).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.parent_item, null);
+//                convertView = (View) getActivity().getLayoutInflater().from(getActivity()).inflate(
+//                        R.layout.parent_item, null);
+        }
+        convertView.setTag(R.layout.parent_item, groupPosition);
+        convertView.setTag(R.layout.child_item, -1);
+        ImageView image = convertView.findViewById(R.id.open_close_parent);
+        ImageView message = convertView.findViewById(R.id.parent_message_icon);
+        TextView text = convertView.findViewById(R.id.parent_title);
+        text.setText(parentList[groupPosition]);
+        message.setImageResource(R.mipmap.ic_message_menu);
+        if(isExpanded){
+            image.setImageResource(R.mipmap.ic_close_menu);
+        }else{
+            image.setImageResource(R.mipmap.ic_open_menu);
+        }
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.child_item, null);
+//                convertView = (View) getActivity().getLayoutInflater().from(getActivity()).inflate(
+//                        R.layout.child_item, null);
+        }
+        convertView.setTag(R.layout.parent_item, groupPosition);
+        convertView.setTag(R.layout.child_item, childPosition);
+        ImageView image = convertView.findViewById(R.id.signal_image);
+        image.setImageResource(R.mipmap.ic_signal);
+        TextView text = convertView.findViewById(R.id.child_title);
+        text.setText(dataset.get(parentList[groupPosition]).get(childPosition).getName());
+//        text.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(context, "点到了内置的textview", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
--- app/src/main/java/com/example/liyuze/cantoolapp/mvp/view/fragment/DataFragment.java
+++ app/src/main/java/com/example/liyuze/cantoolapp/mvp/view/fragment/DataFragment.java
@@ -7,9 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.liyuze.cantoolapp.R;
+import com.example.liyuze.cantoolapp.mvp.adapter.MyExpandableListViewAdapter;
+import com.example.liyuze.cantoolapp.mvp.constants.Constants;
+import com.example.liyuze.cantoolapp.mvp.model.signal;
+import com.example.liyuze.cantoolapp.mvp.view.activity.MainActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +35,19 @@ import com.example.liyuze.cantoolapp.R;
 public class DataFragment extends Fragment {
 
     public static final String TAG = DataFragment.class.toString();
+
+    private ExpandableListView listview;
+    private MyExpandableListViewAdapter adapter;
+
+
+//    private Map<String, List<String>> dataset = new HashMap<>();
+//    private String[] parentList = new String[]{"first", "second", "third"};
+//    private List<String> childrenList1 = new ArrayList<>();
+//    private List<String> childrenList2 = new ArrayList<>();
+//    private List<String> childrenList3 = new ArrayList<>();
+
+    private Map<String, List<signal>> dataset;
+    private String[] parentList = new String[Constants.DATATABLE.size()];
 
     public static DataFragment newInstance(String param1) {
         DataFragment fragment = new DataFragment();
@@ -46,8 +72,50 @@ public class DataFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_data, container, false);
         Bundle bundle = getArguments();
         String agrs1 = bundle.getString("agrs2");
-        TextView tv = (TextView)view.findViewById(R.id.fg_data);
-        tv.setText(agrs1);
+//        TextView tv = (TextView)view.findViewById(R.id.fg_data);
+//        tv.setText(agrs1);
+
+        listview =  view.findViewById(R.id.expandablelistview_data);
+        initialData();
+        adapter = new MyExpandableListViewAdapter(getActivity(),dataset,parentList);
+        listview.setAdapter(adapter);
+        // 表示不使用系统提供的展开和收起的图标
+        listview.setGroupIndicator(null);
+        // 表示默认打开第一列
+//        listView.expandGroup(0);
+
+        listview.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                Toast.makeText(getActivity(), "第"+groupPosition+"组的第"+childPosition+"被点击了", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
         return view;
     }
+
+
+    private void initialData() {
+        dataset = Constants.DATATABLE;
+        Map<String,List<signal>> temp = Constants.DATATABLE;
+        int i =0;
+        for(Map.Entry<String,List<signal>> entry : temp.entrySet()){
+            parentList[i] = entry.getKey();
+            i+=1;
+        }
+//        childrenList1.add(parentList[0] + "-" + "first");
+//        childrenList1.add(parentList[0] + "-" + "second");
+//        childrenList1.add(parentList[0] + "-" + "third");
+//        childrenList2.add(parentList[1] + "-" + "first");
+//        childrenList2.add(parentList[1] + "-" + "second");
+//        childrenList2.add(parentList[1] + "-" + "third");
+//        childrenList3.add(parentList[2] + "-" + "first");
+//        childrenList3.add(parentList[2] + "-" + "second");
+//        childrenList3.add(parentList[2] + "-" + "third");
+//        dataset.put(parentList[0], childrenList1);
+//        dataset.put(parentList[1], childrenList2);
+//        dataset.put(parentList[2], childrenList3);
+    }
+
 }
