e4206aabf090ab0ecf7cc25024c31764137e9d46
Han Zhicheng <han_ven72@163.com>
2017-10-24 12:24:03 +0800

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/liyuze/cantoolapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/liyuze/cantoolapp/ExampleInstrumentedTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 /**
  * Instrumentation test, which will execute on an Android device.
  * It's just a ... 
- *
+ * 
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
